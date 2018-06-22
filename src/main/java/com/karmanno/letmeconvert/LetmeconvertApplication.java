8892361f877bdde6f1d7c06d710a1655837c192b
Karmanov Dmitry <karmanno@gmail.com>
2018-06-22 16:28:31 +1000

    Initial commit

---
+++ src/main/java/com/karmanno/letmeconvert/LetmeconvertApplication.java
@@ -0,0 +1,12 @@
+package com.karmanno.letmeconvert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetmeconvertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetmeconvertApplication.class, args);
+    }
+}
