d3fb3868da5c2bf950ce37e792fc50cc39e93776
Karmanov Dmitry <karmanno@gmail.com>
2018-06-25 18:04:05 +1000

    Added some implementations of FileSystemStorageService class.

--- src/main/java/com/karmanno/letmeconvert/service/FileSystemStorageService.java
+++ src/main/java/com/karmanno/letmeconvert/service/FileSystemStorageService.java
@@ -1,14 +1,19 @@
 package com.karmanno.letmeconvert.service;
 
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 @Service
 public class FileSystemStorageService implements StorageService {
+
+    private ResourceLoader resourceLoader;
 
     @Override
     public void init() {
@@ -27,12 +32,12 @@ public class FileSystemStorageService implements StorageService {
 
     @Override
     public Path load(String filename) {
-        return null;
+        return Paths.get(filename);
     }
 
     @Override
     public Resource loadAsResource(String filename) {
-        return null;
+        return resourceLoader.getResource(filename);
     }
 
     @Override
