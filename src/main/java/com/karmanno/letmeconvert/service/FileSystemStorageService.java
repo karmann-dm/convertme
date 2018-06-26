6d51b59975eb58c31c07c4d63fb3414692da0d6a
Karmanov Dmitry <karmanno@gmail.com>
2018-06-26 16:27:31 +1000

    Fixed implementation according to StorageService interface.

--- src/main/java/com/karmanno/letmeconvert/service/FileSystemStorageService.java
+++ src/main/java/com/karmanno/letmeconvert/service/FileSystemStorageService.java
@@ -1,10 +1,16 @@
 package com.karmanno.letmeconvert.service;
 
+import com.karmanno.letmeconvert.model.UploadedFile;
+import com.karmanno.letmeconvert.repository.UploadedFileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -12,6 +18,8 @@ import java.util.stream.Stream;
 
 @Service
 public class FileSystemStorageService implements StorageService {
+
+    private final static String STORAGE_PATH = "/src/main/java/resources/uploads";
 
     private ResourceLoader resourceLoader;
 
@@ -21,8 +29,14 @@ public class FileSystemStorageService implements StorageService {
     }
 
     @Override
-    public void store(MultipartFile file) {
-
+    public UploadedFile store(MultipartFile file) throws IOException {
+        if(file.isEmpty())
+            return null;
+        Files.copy(file.getInputStream(), Paths.get("src/main/resources/uploads").resolve(file.getOriginalFilename()));
+        UploadedFile uploadedFile = new UploadedFile();
+        uploadedFile.setPathToFile("src/main/resources/uploads/" + file.getOriginalFilename());
+        uploadedFile.setName(file.getOriginalFilename());
+        return uploadedFile;
     }
 
     @Override
@@ -36,8 +50,13 @@ public class FileSystemStorageService implements StorageService {
     }
 
     @Override
-    public Resource loadAsResource(String filename) {
-        return resourceLoader.getResource(filename);
+    public Resource loadAsResource(String filename) throws MalformedURLException {
+        Path file = Paths.get(filename);
+        Resource resource = new UrlResource(file.toUri());
+        if(resource.exists() || resource.isReadable())
+            return resource;
+        else
+            throw new RuntimeException("Failed to load file " + filename + " as resource");
     }
 
     @Override
