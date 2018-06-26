53c19f73b0b25c9cee3c4664b0fb78f5dc29a8bf
Karmanov Dmitry <karmanno@gmail.com>
2018-06-26 16:25:04 +1000

    Changed loadAsResource method, added throwing MalformedURLException and changed return type to Resource.

--- src/main/java/com/karmanno/letmeconvert/service/StorageService.java
+++ src/main/java/com/karmanno/letmeconvert/service/StorageService.java
@@ -4,6 +4,8 @@ import com.karmanno.letmeconvert.model.UploadedFile;
 import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Path;
 import java.util.stream.Stream;
 
@@ -12,6 +14,6 @@ public interface StorageService {
     UploadedFile store(MultipartFile file) throws IOException;
     Stream<Path> loadAll();
     Path load(String filename);
-    Resource loadAsResource(String filename);
+    Resource loadAsResource(String filename) throws MalformedURLException;
     void deleteAll();
 }
