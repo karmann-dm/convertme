951635aab179c3e0647619617ea70021e3183072
Karmanov Dmitry <karmanno@gmail.com>
2018-06-26 14:22:52 +1000

    Fixed entity errors.

--- src/main/java/com/karmanno/letmeconvert/model/UploadedFile.java
+++ src/main/java/com/karmanno/letmeconvert/model/UploadedFile.java
@@ -1,12 +1,23 @@
 package com.karmanno.letmeconvert.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "files")
 public class UploadedFile extends UserDateAudit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "path_to_file")
     private String pathToFile;
+
+    public UploadedFile() {}
+
+    public UploadedFile(String pathToFile) {
+        this.pathToFile = pathToFile;
+    }
 
     public String getPathToFile() {
         return pathToFile;
